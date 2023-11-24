197d8c2de6f47c69ceb0abbbd16d4064bc2566fa
luuhai48 <luuhai.hn48@gmail.com>
2023-11-24 13:42:23 +0700

    Assign id when download

--- download.js
+++ download.js
@@ -11,6 +11,8 @@ const sharp = require("sharp");
 const stream = require("stream");
 const util = require("util");
 const pipeline = util.promisify(stream.pipeline);
+const slug = require("slug");
+const crypto = require("crypto");
 
 const download = async () => {
   const list = await axios
@@ -36,16 +38,27 @@ const download = async () => {
     return;
   }
 
-  fs.mkdirSync(`./downloads/${find.name}`, { recursive: true });
+  const dirName = slug(find.name);
+  fs.mkdirSync(`./downloads/${dirName}`, { recursive: true });
+
+  const pack = {
+    name: find.name,
+    slug: dirName,
+    total: 0,
+    stickers: [],
+  };
 
   await axios.get(find.iconUrl, { responseType: "stream" }).then((response) => {
     response.data.pipe(
-      fs.createWriteStream(`./downloads/${find.name}/thumbnail.png`)
+      fs.createWriteStream(`./downloads/${dirName}/thumbnail.png`)
     );
   });
 
   const ids = res.value.map((s) => new URL(s.url).searchParams.get("eid"));
+  pack.total = ids.length;
+
   for (const id of ids) {
+    console.log(`Downloading file ${id}.png`);
     await axios
       .get(`https://zalo-api.zadn.vn/api/emoticon/sprite?eid=${id}&size=130`, {
         responseType: "stream",
@@ -53,26 +66,33 @@ const download = async () => {
       .then(async (response) => {
         await pipeline(
           response.data,
-          fs.createWriteStream(`./downloads/${find.name}/${id}.png`)
+          fs.createWriteStream(`./downloads/${dirName}/${id}.png`)
         );
 
         await new Promise((resolve) => {
-          sharp(`./downloads/${find.name}/${id}.png`)
+          sharp(`./downloads/${dirName}/${id}.png`)
             .png({ quality: compressImageQualiry })
-            .toFile(`./downloads/${find.name}/${id}-min.png`, function (err) {
+            .toFile(`./downloads/${dirName}/${id}-min.png`, function (err) {
               if (err) {
                 return console.error(err);
               }
               resolve(true);
             });
         });
-        fs.unlinkSync(`./downloads/${find.name}/${id}.png`);
+        fs.unlinkSync(`./downloads/${dirName}/${id}.png`);
         fs.renameSync(
-          `./downloads/${find.name}/${id}-min.png`,
-          `./downloads/${find.name}/${id}.png`
+          `./downloads/${dirName}/${id}-min.png`,
+          `./downloads/${dirName}/${id}.png`
         );
+        pack.stickers.push({
+          id: crypto.randomUUID(),
+          filePath: `${id}.png`,
+          number: parseInt(id),
+        });
       });
   }
+
+  fs.writeFileSync(`./downloads/${dirName}/pack.json`, JSON.stringify(pack));
 };
 
 download();
